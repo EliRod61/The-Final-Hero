bd6798601fe54edf908c32210490056b7c5381d5
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2024-05-11 16:26:20 -0400

    World 1-4 build but platform not moving

---
+++ Hero's_Final_Journey/Assets/Scripts/movingPlatform.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour
+{
+    // Platform starting and end position values
+    public Vector3 myStartPosition = new Vector3(0, 0, -2);
+    public Vector3 myEndPosition = new Vector3(-4, 0, -2);
+    public int speed = 3;
+    public bool forward = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y,gameObject.transform.position.z + 1);
+
+        //Platforms goes backward when end position is reached
+        if (gameObject.transform.position.z >= myEndPosition.z)
+        {
+            forward = false;
+        }
+        //Platform switches forward when reaching start position 
+        if (gameObject.transform.position.z <= myStartPosition.z)
+        {
+            forward = true;
+        }
+
+        //Add speed when moving forward
+        if (forward == true)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + (Time.deltaTime * speed));
+        }
+        //Subtract speed when going backward
+        if (forward == false)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - (Time.deltaTime * speed));
+        }
+    }
+}
